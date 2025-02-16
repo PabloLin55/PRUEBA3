08347071d1ab1826337e4001559cd8e8b60961ca
PabloLin55 <pablo.lin@educa.madrid.org>
2025-02-16 13:23:33 +0100

    Nuevo cambio bucle

--- ProyectoPrueba/src/Main.java
+++ ProyectoPrueba/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("G3HDSHJS");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
 
     }
 }
